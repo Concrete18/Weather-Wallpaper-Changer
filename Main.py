ede20ee0a5c82e2bd8280102b5851c6f41ce1506
Concrete18 <michaelericson19@gmail.com>
2021-01-21 16:35:20 -0500

    deleted main.py

--- Main.py
+++
@@ -1,7 +0,0 @@
-from Weather_Wallpaper_Changer import Weather
-import threading
-
-Main = Weather()
-weather_thread = threading.Thread(target=Main.Run, daemon=True)
-weather_thread.start()
-Main.Create_Tray()
