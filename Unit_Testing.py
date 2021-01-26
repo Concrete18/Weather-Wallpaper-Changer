3d7120e6495bf21927d89d1891675688b6e482df
Concrete18 <michaelericson19@gmail.com>
2021-01-25 21:41:20 -0500

    Added validation to unit test

--- Unit_Testing.py
+++ Unit_Testing.py
@@ -26,6 +26,39 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual(Main.create_url(), zip_url)
 
 
+    def test_validate_entry(self):
+        Main = Weather()
+        # api key
+        self.assertTrue(Main.validate_entry('openweatherapikey', 'ebb08232da61e5c0sht76563a5hf7637'))
+        self.assertFalse(Main.validate_entry('openweatherapikey', '31820'))
+        # temp unit
+        self.assertTrue(Main.validate_entry('temp_unit', 'Fahrenheit'))
+        self.assertTrue(Main.validate_entry('temp_unit', 'Celsius'))
+        self.assertFalse(Main.validate_entry('temp_unit', 'Fahrenheight'))
+        self.assertFalse(Main.validate_entry('temp_unit', '1584525212sdas'))
+        # location mode
+        self.assertTrue(Main.validate_entry('location_mode', 'zip'))
+        self.assertTrue(Main.validate_entry('location_mode', 'coord'))
+        self.assertFalse(Main.validate_entry('location_mode', 'this is wrong'))
+        # latitude
+        self.assertTrue(Main.validate_entry('latitude', '-12.25'))
+        self.assertFalse(Main.validate_entry('latitude', 'this is wrong'))
+        # longitude
+        self.assertTrue(Main.validate_entry('longitude', '12'))
+        self.assertFalse(Main.validate_entry('longitude', 'this is wrong'))
+        # zip code
+        self.assertTrue(Main.validate_entry('zip_code', '12345'))
+        self.assertTrue(Main.validate_entry('zip_code', 12345))
+        self.assertFalse(Main.validate_entry('zip_code', 1234567))
+        self.assertFalse(Main.validate_entry('zip_code', 'this is wrong'))
+        # country code
+        self.assertTrue(Main.validate_entry('zip_code', '31820'))
+        self.assertFalse(Main.validate_entry('zip_code', 'this is wrong'))
+        # check rate
+        self.assertTrue(Main.validate_entry('check_rate', 20))
+        self.assertTrue(Main.validate_entry('check_rate', '20'))
+
+
     # def test_Weather_Check(self):
     #     self.assertEqual(ww.Weather_Check(), '')
 
